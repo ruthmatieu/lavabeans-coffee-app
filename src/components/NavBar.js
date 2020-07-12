a440b090b7378734bdfc4fefac8b1b151e77ca3c
matieuruth <matieu.ruth@gmail.com>
2020-07-12 19:21:14 -0400

    more comps

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,34 @@
-import React from "react";
-import logo from "../img/logo.png";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
-class NavBar extends React.Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <nav className="nav-bar">
+import logo from "../img/logo.png";
+import Order from "./Order";
+
+function NavBar() {
+    return (
+         <div>
+            <header>
+                <nav className="nav-bar">
                         <div class="nav-left">
                             
                             <ul>
                                 <li><img className="logo" src={logo} alt="logo"/></li>
-                                <li><a href="google.com">Order</a></li>
+                                <li><Link to ="/google.com">Order</Link></li>
                                 <li><a href="google.com">Cards</a></li>
                                 <li><a href="google.com">Gift</a></li>
                             </ul>
-                        </div>
-                        <div class="nav-right">
+                    </div>
+                    <div class="nav-right">
                             <ul>
                                 <li><a href="google.com"><button className="btn-two">Join Now</button></a></li>
                                 <li><a href="google.com"><button className="btn-one">Sign In</button></a></li>
                                 <li><a href="google.com"><i class="fas fa-map-marker-alt"></i>Find a store</a></li>
                             </ul>
-                        </div>
-                    </nav>
-                </header>
-            </div>
-        )
-    }
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
 }
 
 export default NavBar
---
+++ src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Order() {
+    return (
+        <div>
+            <h1>Menu</h1>
+            <h2>Mains</h2>
+        </div>
+    )
+}
+
+export default Order
