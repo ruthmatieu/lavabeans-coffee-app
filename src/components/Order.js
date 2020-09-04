a7f5506e6ae2a40c1ee19ddadfd6757ab8d6054e
matieuruth <matieu.ruth@gmail.com>
2020-09-03 20:35:32 -0400

    added navigation links

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MenuItems from './MenuItems';
+import axios from 'axios';
 
 function Order() {
+    const [menuItems, setMenuItems] = useState([]);
+
+    useEffect( () => {
+        //ttps://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+        axios.get(`https://api.jikan.moe/v3/manga/1/characters`)
+        .then(res => {
+            console.log('response:', res)
+            setMenuItems(res.data.characters)
+        })
+        .catch(err => {
+            console.log('error:', err)
+        })
+    },[])
+
     return (
         <div>
             <h1>Menu</h1>
+            {menuItems.map(item => (
+                <div>
+                    <p>{item.name}</p>
+                    <img src={item.image_url} alt={item.name} className="product-image"/>
+                </div>
+            ))}
             <MenuItems menuTitle="Drinks"/>
             <MenuItems menuTitle="Food"/>
             <MenuItems menuTitle="At Home Coffee"/>
