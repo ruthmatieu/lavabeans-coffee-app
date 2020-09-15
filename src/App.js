8c255d1eaf674f9d46a7dcbf08943f5460958516
matieuruth <matieu.ruth@gmail.com>
2020-09-15 14:32:27 -0400

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
 //css
 import './styles.css';
-import NavbarRedo from "./components/Navigation";
+
+//components
+import Homepage from "./components/Homepage";
+import Order from './components/Order';
+import Cards from './components/Cards';
+import Gifts from './components/Gifts';
+import Footer from "./components/Footer";
+
+//logo
+import logo from "./img/logo.png";
 
 function App() {
   return (
+
     <div>
-      <NavbarRedo/> 
+      <div>
+            <nav>
+                <ul className="left-nav">
+                    <Link to="/"><img src={logo} alt="logo" className="logo"/></Link>
+                    <Link to="/order"><li>order</li></Link>
+                    <Link to="/cards"><li>cards</li></Link>
+                    <Link to="gifts"><li>gifts</li></Link>
+                </ul>
+
+                <ul className="right-nav">
+                    <li>Find a store</li>
+                    <button className="nav-btn-one"><a href="#">Sign in</a></button>
+                    <button className="nav-btn-two"><a href="#">Join now</a></button>
+                </ul>
+            </nav>
+            <div>
+                <Route exact path="/" component={Homepage}/>
+                <Route path="/order" component={Order}/>
+                <Route path="/cards" component={Cards}/>
+                <Route path="/gifts" component={Gifts}/>
+            </div>
+            <Footer/>
+        </div>
     </div>
   );
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -16,28 +16,28 @@ function Homepage() {
     const homeData = [
         {
             id: 1,
-            img: {coffeeOne},
+            img: coffeeOne,
             title: "Brighten up with a free drink", 
             description: "For a limited time, get 150 Bonus Stars after your first order when you join Lavabeans Rewards—that's a free drink. Restrictions apply. See Lavabeans.com.", 
             btn: "Join now"
         },
         {
             id: 2,
-            img: {coffeeTwo},
+            img: coffeeTwo,
             title: "Buy one, get one free", 
             description: "Check back for our deal on any handcrafted drink, select Thursdays from 2–7 pm. At participating stores.",
             btn: "Join now"
         },
         {
             id: 3,
-            img: {coffeeThree},
+            img: coffeeThree,
             title: "Order ahead and pay", 
             description: "Find your favorite drink or food item, customize it your way, check out and pick up in select stores.", 
             btn:"Order"
         },
         {
             id: 4,
-            img: {coffeeFour},
+            img: coffeeFour,
             title: "Lavabeans® Delivers on Uber Eat", 
             description: "Enjoy a $0 delivery fee on your Lavabeans favorites for a limited time (other taxes/fees apply).", 
             btn: "Order"
